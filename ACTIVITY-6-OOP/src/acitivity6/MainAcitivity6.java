3873b83954c0096da2c5337984ba8f067a79450b
Jumayca Malilong <malilongjumayca@g.cjc.edu.ph>
2025-06-15 23:31:36 +0800

    Add files via upload

---
+++ ACTIVITY-6-OOP/src/acitivity6/MainAcitivity6.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package acitivity6;
+
+public class MainAcitivity6 {
+
+  
+    public static void main(String[] args) {
+        Inventory open = new Inventory();
+        open.setVisible(true);
+    }
+    
+}
